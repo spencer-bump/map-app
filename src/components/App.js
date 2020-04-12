4880ae1171340b70afb18f915fee7a928ad0327f
Spencer Eldred <eldredspencer@gmail.com>
2020-04-12 04:21:55 -1000

    Add Leaflet css and js tags

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LeafletMap from './LeafletMap';
+
+const App = () => {
+  return (
+    <div className="ui container" >
+      <LeafletMap />
+    </div>
+
+  );
+};
+
+export default App;
---
+++ src/components/LeafletMap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LeafletMap = () => {
+
+
+  return (
+    <div id="mapid">Map goes here</div>
+  )
+
+};
+
+export default LeafletMap;
